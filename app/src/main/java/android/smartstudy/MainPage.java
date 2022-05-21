3667814d88b0aea3c626155cbf536fb73026d04f
zuza571 <zuzanna.pietrzak571@gmail.com>
2022-05-21 20:41:02 +0200

    final

--- app/src/main/java/android/smartstudy/MainPage.java
+++ app/src/main/java/android/smartstudy/MainPage.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
