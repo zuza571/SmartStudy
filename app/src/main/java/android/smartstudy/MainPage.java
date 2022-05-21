8e14ce4aa891130191442ae5322520e09a044721
JakubMazur965 <jakubmazur@student.agh.edu.pl>
2022-05-21 20:27:21 +0200

    koniec

--- app/src/main/java/android/smartstudy/MainPage.java
+++ app/src/main/java/android/smartstudy/MainPage.java
@@ -146,6 +146,7 @@ public class MainPage extends AppCompatActivity {
             }
             else {
                 lessonDayInt = 7 - Math.abs(currentDay - lessonDayInt);
+                lessonDay = lessons.get(i).getDayOfWeek();
             }
             int currentTimeDifference = (lessons.get(i).getStartTime().getHour() - now.getHour()) * 60 + (lessons.get(i).getStartTime().getMinute() - now.getMinute()) + (lessonDayInt * 24 * 60);
 
